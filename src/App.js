12db4ef01e94d417d62295e84ee8940092225b02
brettCole <acodercalledcole@gmail.com>
2019-09-19 22:44:46 -0400

    created components folder and NoteTitle to test text display

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+import './components/NoteTitle';
+import NoteTitle from './components/NoteTitle';
+
 function App() {
   return (
     <div className="App">
-      
+      <NoteTitle />
+      <NoteTitle />
     </div>
   );
 }
